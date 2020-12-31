2ee0b2a7180bb31f8dc612611e8fb8fb04eeaf99
Nikita <baby0benz@gmail.com>
2021-01-01 02:58:28 +0300

    project skeleton and login screen

---
+++ app/src/main/java/com/example/course_project/data/Result.java
@@ -0,0 +1,47 @@
+package com.example.course_project.data;
+
+/**
+ * A generic class that holds a result success w/ data or an error exception.
+ */
+public class Result {
+    // hide the private constructor to limit subclass types (Success, Error)
+    private Result() {}
+
+    // Success sub-class
+    public static final class Success<T> extends Result {
+        private final T data;
+
+        public Success(T data) {
+            this.data = data;
+        }
+
+        public T getData() {
+            return this.data;
+        }
+    }
+
+    // Error sub-class
+    public static final class Error extends Result {
+        private final Exception data;
+
+        public Error(Exception error) {
+            this.data = error;
+        }
+
+        public Exception getError() {
+            return this.data;
+        }
+    }
+
+    @Override
+    public String toString() {
+        if (this instanceof Result.Success) {
+            Result.Success<?> success = (Result.Success<?>) this;
+            return "Success[data=" + success.getData().toString() + "]";
+        } else if (this instanceof Result.Error) {
+            Result.Error error = (Result.Error) this;
+            return "Error[exception=" + error.getError().toString() + "]";
+        }
+        return "";
+    }
+}
