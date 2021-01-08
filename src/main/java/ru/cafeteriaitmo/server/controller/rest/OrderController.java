ee562932fa84c25e185eb531d7dd6ec221ccdf29
NEE_TT <Nshmendyuk.ru@gmail.com>
2021-01-08 06:11:17 +0300

    updated order controller with pagination, creating order in process

--- src/main/java/ru/cafeteriaitmo/server/controller/rest/OrderController.java
+++ src/main/java/ru/cafeteriaitmo/server/controller/rest/OrderController.java
@@ -1,25 +1,34 @@
 package ru.cafeteriaitmo.server.controller.rest;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.json.simple.JSONArray;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import ru.cafeteriaitmo.server.controller.exception.NoEntityException;
 import ru.cafeteriaitmo.server.domain.entity.Order;
-import ru.cafeteriaitmo.server.domain.entity.Product;
-
-import java.util.List;
+import ru.cafeteriaitmo.server.service.OrderService;
 
 @RestController("/api/order")
+@RequiredArgsConstructor
 public class OrderController {
+    private final OrderService orderService;
 
-    @GetMapping
-    public @ResponseBody Order getOrder() {
-        return null;
+    @GetMapping("/{page}")
+    public @ResponseBody Page<Order> getOrderPage(@PathVariable long page) {
+        return orderService.getOrderPage(page);
     }
 
-    @PostMapping
-    public void doOrder(List<Product> products) {
+    @GetMapping
+    public Order getOrder(@RequestParam long orderId) throws NoEntityException {
+        return orderService.getOrder(orderId).orElseThrow(() -> new NoEntityException(Order.class.getSimpleName().toLowerCase(), orderId));
+    }
 
+    @ResponseStatus(value = HttpStatus.CREATED)
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Order doOrder(@RequestBody JSONArray valuesToCreateOrder) {
+        return orderService.createOrder(valuesToCreateOrder);
     }
 
 }
