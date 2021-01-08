14e8e54e1721f43013ab9bb8a5f56f70e118bcd9
NEE_TT <Nshmendyuk.ru@gmail.com>
2021-01-08 06:12:20 +0300

    updated repositories

--- src/main/java/ru/cafeteriaitmo/server/repository/OrderRepository.java
+++ src/main/java/ru/cafeteriaitmo/server/repository/OrderRepository.java
@@ -1,7 +1,9 @@
 package ru.cafeteriaitmo.server.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.cafeteriaitmo.server.domain.entity.Order;
 
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 }
--- src/main/java/ru/cafeteriaitmo/server/repository/ProductRepository.java
+++ src/main/java/ru/cafeteriaitmo/server/repository/ProductRepository.java
@@ -1,7 +1,14 @@
 package ru.cafeteriaitmo.server.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import ru.cafeteriaitmo.server.domain.entity.Building;
 import ru.cafeteriaitmo.server.domain.entity.Product;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Page<Product> findAllProductsByBuilding(Building building, Pageable pageable);
 }
