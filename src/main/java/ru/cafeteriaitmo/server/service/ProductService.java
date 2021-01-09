6e20fb5a688cea5ae6d1d5902a320908686fc295
NEE_TT <Nshmendyuk.ru@gmail.com>
2021-01-09 06:18:58 +0300

    added method for save product in db

--- src/main/java/ru/cafeteriaitmo/server/service/ProductService.java
+++ src/main/java/ru/cafeteriaitmo/server/service/ProductService.java
@@ -1,5 +1,6 @@
 package ru.cafeteriaitmo.server.service;
 
+import org.json.simple.JSONObject;
 import org.springframework.data.domain.Page;
 import ru.cafeteriaitmo.server.controller.exception.NoEntityException;
 import ru.cafeteriaitmo.server.domain.entity.Product;
@@ -8,4 +9,5 @@ public interface ProductService {
     Page<Product> getPageFromBuilding (String building, Integer page) throws NoEntityException;
     Page<Product> getProductPage(Long page);
     Product getProduct(Long id) throws NoEntityException;
+    Product addProduct(JSONObject productJson);
 }
