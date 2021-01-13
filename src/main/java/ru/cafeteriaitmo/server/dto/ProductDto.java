243c9f0080dd624f395abf51271be5215b9e140d
Nikolay <Nshmendyuk.ru@yandex.ru>
2021-01-14 04:17:25 +0700

    added fields as image, name of building and id

--- src/main/java/ru/cafeteriaitmo/server/dto/ProductDto.java
+++ src/main/java/ru/cafeteriaitmo/server/dto/ProductDto.java
@@ -7,10 +7,13 @@ import java.io.Serializable;
 @Data
 public class ProductDto implements Serializable {
     private static final long serialVersionUID = 2116976449580433258L;
+    private Long id;
     private String name;
     private Float price;
     private Boolean available;
     private String description;
     private String type;
+
     private byte[] image;
+    private String nameBuilding;
 }
