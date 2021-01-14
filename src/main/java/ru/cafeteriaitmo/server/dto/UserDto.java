c91c4ea638832b5dd201634acea177d8949643a7
Nikolay <Nshmendyuk.ru@yandex.ru>
2021-01-14 07:30:03 +0700

    added uid - optional

--- src/main/java/ru/cafeteriaitmo/server/dto/UserDto.java
+++ src/main/java/ru/cafeteriaitmo/server/dto/UserDto.java
@@ -7,7 +7,7 @@ import java.io.Serializable;
 @Data
 public class UserDto implements Serializable {
     private static final long serialVersionUID = 3040401765434495059L;
-    private String monitorCode;
+    private String uid;
     private String name;
     private String surname;
 }
