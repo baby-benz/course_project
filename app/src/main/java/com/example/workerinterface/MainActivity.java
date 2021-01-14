cd17fee7931778a9bac7d1d802fa48b45c2ae96b
Alexey <lexanus686@gmail.com>
2021-01-14 19:58:06 +0300

    Network connection to server

--- app/src/main/java/com/example/workerinterface/MainActivity.java
+++ app/src/main/java/com/example/workerinterface/MainActivity.java
@@ -54,7 +54,6 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
-
     }
 
     @Override
--- app/src/main/java/com/example/workerinterface/ui/menu/MenuActivity.java
+++ app/src/main/java/com/example/workerinterface/ui/menu/MenuActivity.java
@@ -3,13 +3,29 @@ package com.example.workerinterface.ui.menu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+
+import android.net.http.HttpResponseCache;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
+import android.widget.TextView;
 
 import com.example.workerinterface.R;
 
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,8 +46,68 @@ public class MenuActivity extends AppCompatActivity {
         //находим наш linear который у нас под кнопкой add edittext в activity_main.xml
         final GridLayout table = (GridLayout) findViewById(R.id.table);
 
-        addButton.setOnClickListener(v -> {
+        new Thread(new Runnable() {
 
+            @Override
+            public void run() {
+                try  {
+                    String IP = "192.168.1.130:8080";
+                    URL url = null;
+                    try {
+                        url = new URL(new URL("http",IP, "/api/product/0").toString().replace("[","").replace("]",""));
+                        System.out.println(url);
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    }
+                    HttpURLConnection connection = null;
+                    try {
+                        connection = (HttpURLConnection)url.openConnection();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                            try {
+                                InputStream in = new BufferedInputStream(connection.getInputStream());
+                                BufferedReader r = new BufferedReader(new InputStreamReader(in));
+                                StringBuilder total = new StringBuilder();
+                                for (String line; (line = r.readLine()) != null; ) {
+                                    System.out.println(line);
+                                    total.append(line).append('\n');
+                                }
+                                JSONObject jsonObj = new JSONObject(total.toString());
+                                System.out.println(jsonObj);
+                                System.out.println(jsonObj.getJSONArray("content"));
+                                JSONArray jsonarray = jsonObj.getJSONArray("content");
+                                for (int i = 0; i < jsonarray.length(); i++) {
+                                    System.out.println(jsonarray.getJSONObject(i).get("id"));
+                                    System.out.println(jsonarray.getJSONObject(i).get("name"));
+                                    System.out.println(jsonarray.getJSONObject(i).get("price"));
+                                    System.out.println(jsonarray.getJSONObject(i).get("available"));
+                                    System.out.println(jsonarray.getJSONObject(i).get("description"));
+                                }
+                            } finally {
+                                connection.disconnect();
+                            }
+                        }
+                    } catch (IOException e) {
+                        System.out.println("ERROR");
+                        e.printStackTrace();
+                    } finally {
+                        try {
+                            System.out.println(connection.getResponseCode());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+
+        addButton.setOnClickListener(v -> {
             //берем наш кастомный лейаут находим через него все наши кнопки и едит тексты, задаем нужные данные
             @SuppressLint("InflateParams") final View view = getLayoutInflater().inflate(R.layout.custom_food_menu_item, null);
 
@@ -41,4 +117,6 @@ public class MenuActivity extends AppCompatActivity {
             table.addView(view);
         });
     }
+
+
 }
