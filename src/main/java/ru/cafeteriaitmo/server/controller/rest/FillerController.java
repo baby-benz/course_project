367d81634654af1c724be9f742fab997b1e4bced
Nikolay <Nshmendyuk.ru@yandex.ru>
2021-01-14 20:52:15 +0700

    added one order filler

--- src/main/java/ru/cafeteriaitmo/server/controller/rest/FillerController.java
+++ src/main/java/ru/cafeteriaitmo/server/controller/rest/FillerController.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.cafeteriaitmo.server.filler.database.OneOrderDataFiller;
 import ru.cafeteriaitmo.server.filler.database.OrderDataFiller;
 import ru.cafeteriaitmo.server.filler.database.ProductDataFiller;
 
@@ -15,11 +16,13 @@ import ru.cafeteriaitmo.server.filler.database.ProductDataFiller;
 public class FillerController {
     private final ProductDataFiller productDataFiller;
     private final OrderDataFiller orderDataFiller;
+    private final OneOrderDataFiller oneOrderDataFiller;
 
     @GetMapping
     public String addTestData() {
         productDataFiller.createDailyMenuExample();
         orderDataFiller.createUsersOrdering();
+//        oneOrderDataFiller.createOneOrder();
         return "DataBase now contain some products and orders";
     }
 
