f0ae5d0bae4d494200c0d1ea3e50498269ab89c6
Alexey <lexanus686@gmail.com>
2021-01-17 19:00:41 +0300

    Added ViewFlipper to menu + Detailed description of product item

--- app/src/main/java/com/example/workerinterface/ui/menu/MenuActivity.java
+++ app/src/main/java/com/example/workerinterface/ui/menu/MenuActivity.java
@@ -6,13 +6,14 @@ import android.annotation.SuppressLint;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.ViewFlipper;
 
 import com.example.workerinterface.R;
 import com.example.workerinterface.dto.ProductDTO;
@@ -23,34 +24,34 @@ import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Base64;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class MenuActivity extends AppCompatActivity {
 
     //Создаем список вьюх которые будут создаваться
-    private List<View> allItems;
+    private List<View> allItems, allDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        Button addButton = (Button) findViewById(R.id.test_product_button);
+        ViewFlipper vf = findViewById( R.id.viewFlipper );
+
+        Button addButton = findViewById(R.id.test_product_button);
         //инициализировали наш массив с edittext.aьи
         allItems = new ArrayList<>();
         ArrayList<ProductDTO> productDTOS = new ArrayList<>();
 
         //находим наш linear который у нас под кнопкой add edittext в activity_main.xml
-        final GridLayout table = (GridLayout) findViewById(R.id.table);
+        final GridLayout table = findViewById(R.id.table);
 
         new Thread(() -> {
             try  {
@@ -98,6 +99,8 @@ public class MenuActivity extends AppCompatActivity {
 
         AtomicInteger counter = new AtomicInteger();
         addButton.setOnClickListener(v -> {
+            System.out.println(productDTOS.size());
+            System.out.println(this);
             for (int i = 0; i < productDTOS.size(); i++) {
                 System.out.println(i + " " + productDTOS.get(i).getName() + " " + productDTOS.get(i).getNameBuilding());
 
@@ -109,13 +112,48 @@ public class MenuActivity extends AppCompatActivity {
             //добавляем елементы в linearlayout
             table.addView(view);
 
-            TextView tv1 = (TextView) findViewById(R.id.test_text);
-            ImageView iv1 = (ImageView) findViewById(R.id.icon);
+            TextView tv1 = findViewById(R.id.test_text);
+            ImageView iv1 = findViewById(R.id.icon);
 
             byte[] imageBytes = productDTOS.get(i).getImage();
             Bitmap bitmap = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
             iv1.setId(counter.get());
             iv1.setImageBitmap(bitmap);
+            iv1.setMinimumWidth(bitmap.getWidth());
+            iv1.setMinimumHeight(bitmap.getHeight());
+            iv1.setMaxHeight(bitmap.getHeight());
+            iv1.setMaxWidth(bitmap.getWidth());
+
+            iv1.setOnClickListener(v1 -> {
+                System.out.println("Listen" + iv1.getId());
+
+                vf.showNext();
+
+                RelativeLayout layoutDescription = findViewById(R.id.description);
+                @SuppressLint("InflateParams") View descriptionView = getLayoutInflater().inflate(R.layout.custom_food_menu_descirption, null);
+
+                layoutDescription.addView(descriptionView);
+
+                TextView description = findViewById(R.id.text_description_descValue);
+                description.setText(productDTOS.get(iv1.getId()).getDescription());
+
+                TextView name = findViewById(R.id.text_description_nameValue);
+                name.setText(productDTOS.get(iv1.getId()).getName());
+
+                TextView available = findViewById(R.id.text_description_availableValue);
+                available.setText(productDTOS.get(iv1.getId()).getAvailable().toString());
+
+                ImageView image = findViewById(R.id.description_image);
+                image.setImageBitmap(bitmap);
+
+                Button returnButton = findViewById(R.id.back_button);
+                returnButton.setOnClickListener(v2 -> {
+                    System.out.println(this);
+                    System.out.println(this.getIntent());
+                    vf.showPrevious();
+                });
+
+            });
 
             tv1.setId(counter.get());
             tv1.setText(productDTOS.get(counter.get()).getName());
