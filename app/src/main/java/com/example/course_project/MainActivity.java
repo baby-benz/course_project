57cf1073f07109a219d651efe8a661e778043474
Nikita <baby0benz@gmail.com>
2021-01-24 22:53:05 +0300

    replace app theme with theme with no appbar

--- app/src/main/java/com/example/course_project/MainActivity.java
+++ app/src/main/java/com/example/course_project/MainActivity.java
@@ -44,7 +44,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
 
         FragmentManager fragmentManager = getSupportFragmentManager();
