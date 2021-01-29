895edcfd16b4f678ddb65331384ce61e6519ca85
Nikolay <Nshmendyuk.ru@yandex.ru>
2021-01-29 11:45:17 +0700

    added order dto

---
+++ app/src/main/java/com/example/course_project/dto/OrderDto.java
@@ -0,0 +1,21 @@
+package com.example.course_project.dto;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@RequiredArgsConstructor
+public class OrderDto implements Serializable {
+    private static final long serialVersionUID = 8940694154955849897L;
+    private Long id;
+    private LocalDateTime orderedOn;
+    private String monitorCode;
+    private String status;
+
+    private ArrayList<Long> productIds;
+    private String buildingName;
+    private Long userId;
+}
